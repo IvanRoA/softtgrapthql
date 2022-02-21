3ad632a576f1467c78770b2d523fcda75bd2d082
IvanRoA <100147551+IvanRoA@users.noreply.github.com>
2022-02-21 12:04:46 -0600

    Update and rename ClienteQueryResolver.java to QueryResolver.java

--- grapthql/src/main/java/com/ivra/graphql/graphql/resolvers/QueryResolver.java
+++ grapthql/src/main/java/com/ivra/graphql/graphql/resolvers/QueryResolver.java
@@ -7,15 +7,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
-
+import com.ivra.graphql.graphql.model.Banco;
 import com.ivra.graphql.graphql.model.Cliente;
+import com.ivra.graphql.graphql.service.BancoService;
 import com.ivra.graphql.graphql.service.ClienteService;
 
 @Component
-public class ClienteQueryResolver implements GraphQLQueryResolver {
+public class QueryResolver implements GraphQLQueryResolver {
 
 	@Autowired
 	private ClienteService ClienteService;
+	
+	@Autowired
+	private BancoService bancoService;
 	
 	public List<Cliente> clientes(final int registros) {
 		return ClienteService.clientes(registros);
@@ -23,5 +27,13 @@ public class ClienteQueryResolver implements GraphQLQueryResolver {
 	
 	public Optional<Cliente> cliente(final int id) {
 		return ClienteService.cliente(id);
+	}
+	
+	public Optional<Banco> banco(final int id){
+		return bancoService.banco(id);
+	}
+	
+	public List<Banco> bancos(final int registros){
+		return bancoService.bancos(registros);
 	}
 }
