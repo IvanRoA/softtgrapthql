2aa9fd8cc6cedb1da6647ba70d21774d4342aff6
Ivan Rodriguez <ivan2909@hotmail.com>
2022-03-01 13:01:30 -0600

    Se corrige url para ver la documentacion de graphql autogenerada

--- grapthql/src/main/resources/data.sql
+++ grapthql/src/main/resources/data.sql
@@ -1,35 +1,34 @@
 
 
 
-insert into CATALOGOD (SCREEN_DESC,SHORT_DESC,PROCESS_DATE,ABBREVIATION,FULL_DESC) values('BOLIVAR VENE','BOLIVAR DE VENEZUELA','0039','BOLIV','BOLIVAR DE VENEZUELA');
-insert into CATALOGOD (SCREEN_DESC,SHORT_DESC,PROCESS_DATE,ABBREVIATION,FULL_DESC) values('PLATA NO AMO','PLATA NO AMONEDADA','0014','PLATA','PLATA NO AMONENADA');
-insert into CATALOGOD (SCREEN_DESC,SHORT_DESC,PROCESS_DATE,ABBREVIATION,FULL_DESC) values('COLON SALVAD','COLON SALVADORENO','0022','COLON','COLON SALVADORENO');
-insert into CATALOGOD (SCREEN_DESC,SHORT_DESC,PROCESS_DATE,ABBREVIATION,FULL_DESC) values('FRANCO BELGA','FRANCO BELGA','0025','FRANC','FRANCO BELGA');
-insert into CATALOGOD (SCREEN_DESC,SHORT_DESC,PROCESS_DATE,ABBREVIATION,FULL_DESC) values('DOLAR TAIWAN','DOLAR TAIWUANES','0087','DOLAR','DOLAR TAIWANES');
-insert into CATALOGOD (SCREEN_DESC,SHORT_DESC,PROCESS_DATE,ABBREVIATION,FULL_DESC) values('DOLAR FIJI','DOLAR FIJI','0070','DOLAR','DOLAR FIJI');
-insert into CATALOGOD (SCREEN_DESC,SHORT_DESC,PROCESS_DATE,ABBREVIATION,FULL_DESC) values('DOLAR NVAZEL','DOLAR NEOZELANDES','0058','DOLAR','DOLAR NEOZELANDEZ');
-insert into CATALOGOD (SCREEN_DESC,SHORT_DESC,PROCESS_DATE,ABBREVIATION,FULL_DESC) values('DOLAR GUYANA','DOLAR GUYANA','0073','DOLAR','DOLAR GUYANA');
-insert into CATALOGOD (SCREEN_DESC,SHORT_DESC,PROCESS_DATE,ABBREVIATION,FULL_DESC) values('DOLAR SINGAP','DOLAR SINGAPUR','055','DOLAR','DOLAR SINGAPUR');
-commit;
+insert into catalogod (screen_desc,short_desc,process_date,abbreviation,full_desc) values('BOLIVAR VENE','BOLIVAR DE VENEZUELA','0039','BOLIV','BOLIVAR DE VENEZUELA');
+insert into catalogod (screen_desc,short_desc,process_date,abbreviation,full_desc) values('PLATA NO AMO','PLATA NO AMONEDADA','0014','PLATA','PLATA NO AMONENADA');
+insert into catalogod (screen_desc,short_desc,process_date,abbreviation,full_desc) values('COLON SALVAD','COLON SALVADORENO','0022','COLON','COLON SALVADORENO');
+insert into catalogod (screen_desc,short_desc,process_date,abbreviation,full_desc) values('FRANCO BELGA','FRANCO BELGA','0025','FRANC','FRANCO BELGA');
+insert into catalogod (screen_desc,short_desc,process_date,abbreviation,full_desc) values('DOLAR TAIWAN','DOLAR TAIWUANES','0087','DOLAR','DOLAR TAIWANES');
+insert into catalogod (screen_desc,short_desc,process_date,abbreviation,full_desc) values('DOLAR FIJI','DOLAR FIJI','0070','DOLAR','DOLAR FIJI');
+insert into catalogod (screen_desc,short_desc,process_date,abbreviation,full_desc) values('DOLAR NVAZEL','DOLAR NEOZELANDES','0058','DOLAR','DOLAR NEOZELANDEZ');
+insert into catalogod (screen_desc,short_desc,process_date,abbreviation,full_desc) values('DOLAR GUYANA','DOLAR GUYANA','0073','DOLAR','DOLAR GUYANA');
+insert into catalogod (screen_desc,short_desc,process_date,abbreviation,full_desc) values('DOLAR SINGAP','DOLAR SINGAPUR','055','DOLAR','DOLAR SINGAPUR');
 
 
-insert into CATALOGOT (HORA,MONEDA,PRECIO,SIEFORE,VALOR,COL2,COL3,COL4,COL5,COL6,COL7,COL8,COL9,CVE_IVA,TASA_IVA,ISR,INDICE,REGION,PLAZO,T_BRUTA,LIM_INF,LIM_SUP,DIV_MET,DENOMIN,VENTA,CVE_TC,COMPRA,DIARIO,MENSUAL,ANUAL)
+insert into catalogot (hora,moneda,precio,siefore,valor,col2,col3,col4,col5,col6,col7,col8,col9,cve_iva,tasa_iva,isr,indice,region,plazo,t_bruta,lim_inf,lim_sup,div_met,denomin,venta,cve_tc,compra,diario,mensual,anual)
 values('12:23:45','MN','23.56','MBX12345','24','001','002','003','004','005','006','007','009','02','16','28','04','09','12','12','0','1500','MCD','DLS','21.13','012','19.67','03','04','07');
-insert into CATALOGOT (HORA,MONEDA,PRECIO,SIEFORE,VALOR,COL2,COL3,COL4,COL5,COL6,COL7,COL8,COL9,CVE_IVA,TASA_IVA,ISR,INDICE,REGION,PLAZO,T_BRUTA,LIM_INF,LIM_SUP,DIV_MET,DENOMIN,VENTA,CVE_TC,COMPRA,DIARIO,MENSUAL,ANUAL)
+insert into catalogot (hora,moneda,precio,siefore,valor,col2,col3,col4,col5,col6,col7,col8,col9,cve_iva,tasa_iva,isr,indice,region,plazo,t_bruta,lim_inf,lim_sup,div_met,denomin,venta,cve_tc,compra,diario,mensual,anual)
 values('01:34:56','EU','23.56','PDS23678','18','001','002','003','004','005','006','007','009','03','23','24','04','09','12','12','0','1269','MCD','EUR','26.27','012','25.78','03','04','07');
-insert into CATALOGOT (HORA,MONEDA,PRECIO,SIEFORE,VALOR,COL2,COL3,COL4,COL5,COL6,COL7,COL8,COL9,CVE_IVA,TASA_IVA,ISR,INDICE,REGION,PLAZO,T_BRUTA,LIM_INF,LIM_SUP,DIV_MET,DENOMIN,VENTA,CVE_TC,COMPRA,DIARIO,MENSUAL,ANUAL)
+insert into catalogot (hora,moneda,precio,siefore,valor,col2,col3,col4,col5,col6,col7,col8,col9,cve_iva,tasa_iva,isr,indice,region,plazo,t_bruta,lim_inf,lim_sup,div_met,denomin,venta,cve_tc,compra,diario,mensual,anual)
 values('03:45:12','MN','23.56','XXM16934','23','001','002','003','004','005','006','007','009','07','16','28','04','09','12','12','0','2365','MCD','DLS','21.13','012','19.67','03','04','07');
-insert into CATALOGOT (HORA,MONEDA,PRECIO,SIEFORE,VALOR,COL2,COL3,COL4,COL5,COL6,COL7,COL8,COL9,CVE_IVA,TASA_IVA,ISR,INDICE,REGION,PLAZO,T_BRUTA,LIM_INF,LIM_SUP,DIV_MET,DENOMIN,VENTA,CVE_TC,COMPRA,DIARIO,MENSUAL,ANUAL)
+insert into catalogot (hora,moneda,precio,siefore,valor,col2,col3,col4,col5,col6,col7,col8,col9,cve_iva,tasa_iva,isr,indice,region,plazo,t_bruta,lim_inf,lim_sup,div_met,denomin,venta,cve_tc,compra,diario,mensual,anual)
 values('16:49:23','DL','23.56','MBX12345','89','001','002','003','004','005','006','007','009','05','16','28','04','09','12','12','0','2867','MCD','DLS','21.13','012','19.67','03','04','07');
-insert into CATALOGOT (HORA,MONEDA,PRECIO,SIEFORE,VALOR,COL2,COL3,COL4,COL5,COL6,COL7,COL8,COL9,CVE_IVA,TASA_IVA,ISR,INDICE,REGION,PLAZO,T_BRUTA,LIM_INF,LIM_SUP,DIV_MET,DENOMIN,VENTA,CVE_TC,COMPRA,DIARIO,MENSUAL,ANUAL)
+insert into catalogot (hora,moneda,precio,siefore,valor,col2,col3,col4,col5,col6,col7,col8,col9,cve_iva,tasa_iva,isr,indice,region,plazo,t_bruta,lim_inf,lim_sup,div_met,denomin,venta,cve_tc,compra,diario,mensual,anual)
 values('04:23:01','EU','23.56','PDS23678','12','001','002','003','004','005','006','007','009','09','23','24','04','09','12','12','0','3678','MCD','EUR','26.27','012','25.78','03','04','07');
-insert into CATALOGOT (HORA,MONEDA,PRECIO,SIEFORE,VALOR,COL2,COL3,COL4,COL5,COL6,COL7,COL8,COL9,CVE_IVA,TASA_IVA,ISR,INDICE,REGION,PLAZO,T_BRUTA,LIM_INF,LIM_SUP,DIV_MET,DENOMIN,VENTA,CVE_TC,COMPRA,DIARIO,MENSUAL,ANUAL)
+insert into catalogot (hora,moneda,precio,siefore,valor,col2,col3,col4,col5,col6,col7,col8,col9,cve_iva,tasa_iva,isr,indice,region,plazo,t_bruta,lim_inf,lim_sup,div_met,denomin,venta,cve_tc,compra,diario,mensual,anual)
 values('19:35:11','MN','23.56','LNS14598','58','001','002','003','004','005','006','007','009','02','16','28','04','09','12','12','0','6987','MCD','DLS','21.13','012','19.67','03','04','07');
-insert into CATALOGOT (HORA,MONEDA,PRECIO,SIEFORE,VALOR,COL2,COL3,COL4,COL5,COL6,COL7,COL8,COL9,CVE_IVA,TASA_IVA,ISR,INDICE,REGION,PLAZO,T_BRUTA,LIM_INF,LIM_SUP,DIV_MET,DENOMIN,VENTA,CVE_TC,COMPRA,DIARIO,MENSUAL,ANUAL)
+insert into catalogot (hora,moneda,precio,siefore,valor,col2,col3,col4,col5,col6,col7,col8,col9,cve_iva,tasa_iva,isr,indice,region,plazo,t_bruta,lim_inf,lim_sup,div_met,denomin,venta,cve_tc,compra,diario,mensual,anual)
 values('07:17:34','MN','23.56','PRT04576','23','001','002','003','004','005','006','007','009','03','16','28','04','09','12','12','0','2000','MCD','DLS','21.13','012','19.67','03','04','07');
-insert into CATALOGOT (HORA,MONEDA,PRECIO,SIEFORE,VALOR,COL2,COL3,COL4,COL5,COL6,COL7,COL8,COL9,CVE_IVA,TASA_IVA,ISR,INDICE,REGION,PLAZO,T_BRUTA,LIM_INF,LIM_SUP,DIV_MET,DENOMIN,VENTA,CVE_TC,COMPRA,DIARIO,MENSUAL,ANUAL)
+insert into catalogot (hora,moneda,precio,siefore,valor,col2,col3,col4,col5,col6,col7,col8,col9,cve_iva,tasa_iva,isr,indice,region,plazo,t_bruta,lim_inf,lim_sup,div_met,denomin,venta,cve_tc,compra,diario,mensual,anual)
 values('18:47:23','DL','23.56','MBX12345','98','001','002','003','004','005','006','007','009','01','10','19','04','09','12','12','0','2789','MCD','DLS','21.13','012','19.67','03','04','07');
-insert into CATALOGOT (HORA,MONEDA,PRECIO,SIEFORE,VALOR,COL2,COL3,COL4,COL5,COL6,COL7,COL8,COL9,CVE_IVA,TASA_IVA,ISR,INDICE,REGION,PLAZO,T_BRUTA,LIM_INF,LIM_SUP,DIV_MET,DENOMIN,VENTA,CVE_TC,COMPRA,DIARIO,MENSUAL,ANUAL)
+insert into catalogot (hora,moneda,precio,siefore,valor,col2,col3,col4,col5,col6,col7,col8,col9,cve_iva,tasa_iva,isr,indice,region,plazo,t_bruta,lim_inf,lim_sup,div_met,denomin,venta,cve_tc,compra,diario,mensual,anual)
 values('11:59:59','MN','23.56','MBX12345','35','001','002','003','004','005','006','007','009','08','16','28','04','09','12','12','0','3700','MCD','DLS','21.13','012','19.67','03','04','07');
-insert into CATALOGOT (HORA,MONEDA,PRECIO,SIEFORE,VALOR,COL2,COL3,COL4,COL5,COL6,COL7,COL8,COL9,CVE_IVA,TASA_IVA,ISR,INDICE,REGION,PLAZO,T_BRUTA,LIM_INF,LIM_SUP,DIV_MET,DENOMIN,VENTA,CVE_TC,COMPRA,DIARIO,MENSUAL,ANUAL)
+insert into catalogot (hora,moneda,precio,siefore,valor,col2,col3,col4,col5,col6,col7,col8,col9,cve_iva,tasa_iva,isr,indice,region,plazo,t_bruta,lim_inf,lim_sup,div_met,denomin,venta,cve_tc,compra,diario,mensual,anual)
 values('17:27:56','YN','23.56','LNS14598','76','001','002','003','004','005','006','007','009','09','23','24','04','09','12','12','0','4000','MCD','YNS','40.16','012','38.34','03','04','07');
